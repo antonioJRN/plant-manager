9743c8355f05ffc5d43ce5eaf7a2abc1d9286398
antonioJRN <netinhorego13@gmail.com>
2021-04-22 23:15:28 -0300

    Filter&Loading

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList, ActivityIndicator } from 'react-native';
 import { EnviromentButton } from '../components/EnviromentButton';
 import { Header } from '../components/Header';
 import { PlantCardPrimary } from '../components/PlantCardPrimary';
+import { Load } from '../components/Load';
 import api from '../services/api';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
@@ -28,10 +29,53 @@ interface PlantProps {
 export function PlantSelect() {
   const [enviroments, setEnviroments] = useState<EnviromentProps[]>([]);
   const [plants, setPlants] = useState<PlantProps[]>([]);
+  const [filteredPlants, setFilteredPlants] = useState<PlantProps[]>([]);
+  const [enviromentSelected, setEnviromentSelected] = useState('all');
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [loadedAll, setLoadedAll] = useState(false);
+
+  function handleEnviromentSelected(enviroment: string) {
+    setEnviromentSelected(enviroment);
+
+    if (enviroment == 'all')
+    return setFilteredPlants(plants);
+
+    const filtered = plants.filter(plant => plant.environments.includes(enviroment))
+    setFilteredPlants(filtered);
+  }
+
+  async function fetchPlants() {
+    const { data } = await api.get(`plants?_sort=name&_order=asc&_page=${page}&_limit=8`);
+
+    if(!data)
+      return setLoading(true);
+
+    if(page > 1) {
+      setPlants(oldValue => [...oldValue, ...data])
+      setFilteredPlants(oldValue => [...oldValue, ...data])
+    } else {
+      setPlants(data);
+      setFilteredPlants(data);
+    }
+    
+    setLoading(false);
+    setLoadingMore(false);
+  }
+
+  function handleFetchMore(distance: number) {
+    if(distance < 1)
+      return;
+    
+    setLoadingMore(true);
+    setPage(oldValue => oldValue + 1);
+    fetchPlants();
+  }
 
   useEffect(() => {
     async function fetchEnviroment() {
-      const { data } = await api.get('plants_environments');
+      const { data } = await api.get('plants_environments?_sort=title&_order=asc');
       setEnviroments([
         {
           key: 'all',
@@ -45,14 +89,11 @@ export function PlantSelect() {
   }, [])
 
   useEffect(() => {
-    async function fetchPlants() {
-      const { data } = await api.get('plants');
-      setPlants(data);
-    }
-
     fetchPlants();
   }, [])
 
+  if(loading) 
+    return <Load />
   return(
     <View style={styles.container}>
       <View style={styles.header}>
@@ -66,7 +107,11 @@ export function PlantSelect() {
         <FlatList 
           data={enviroments}
           renderItem={({ item }) => (
-            <EnviromentButton title={item.title} />
+            <EnviromentButton 
+              title={item.title} 
+              active={item.key === enviromentSelected} 
+              onPress={() => handleEnviromentSelected(item.key)}
+            />
           )}
           horizontal
           showsHorizontalScrollIndicator={false}
@@ -76,10 +121,19 @@ export function PlantSelect() {
 
       <View style={styles.plants}>
         <FlatList 
-          data={plants}
+          data={filteredPlants}
           renderItem={({ item }) => (
             <PlantCardPrimary data={item} />
           )}
+          showsVerticalScrollIndicator={false}
+          numColumns={2}
+          onEndReachedThreshold={0.1}
+          onEndReached={({distanceFromEnd }) => handleFetchMore(distanceFromEnd)}
+          ListFooterComponent={
+            loadingMore ?
+            <ActivityIndicator color={colors.green} />
+            : <></>
+          }
         />
 
       </View>
@@ -123,5 +177,6 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 32,
     justifyContent: 'center'
-  }
+  },
+
 });
